4fca12a6d65ac3ce12a866f6c803536674c4fe09
Rex Li <iamacuteboyin302@gmail.com>
2020-10-29 21:07:33 +0800

    feat: add start tasks at entry point

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import { config } from './configuration';
 import express from "express";
 import expressHandlebars from "express-handlebars"
 import PageRouter from './page-routers';
+import { setTasks } from './services/task.svc';
 
 const app = express();
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: "" }));
 app.set('view engine', 'handlebars');
-
 app.use('/', PageRouter);
-
+setTasks();
 app.listen(config.appPort, () => {
     console.log(`server listen on ${config.appPort}`)
 })
+
